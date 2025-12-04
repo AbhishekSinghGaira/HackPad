513a4aeea88bfa74a5177192e2ce4e233aa16b1b
AbhishekSinghGaira <gairaabhishek32@gmail.com>
2025-12-04 22:10:11 +0530

    major change

--- main.py
+++ main.py
@@ -1,35 +1,55 @@
-# You import all the IOs of your board
 import board
-
-# These are imports from the kmk library
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.scanners.keypad import KeysScanner
 from kmk.keys import KC
 from kmk.modules.macros import Press, Release, Tap, Macros
 
-# This is the main instance of your keyboard
 keyboard = KMKKeyboard()
 
-# Add the macro extension
 macros = Macros()
 keyboard.modules.append(macros)
 
-# Define your pins here!
 PINS = [board.D3, board.D4, board.D2, board.D1]
 
-# Tell kmk we are not using a key matrix
 keyboard.matrix = KeysScanner(
     pins=PINS,
     value_when_pressed=False,
 )
 
-# Here you define the buttons corresponding to the pins
-# Look here for keycodes: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/keycodes.md
-# And here for macros: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/macros.md
 keyboard.keymap = [
-    [KC.A, KC.DELETE, KC.MACRO("Hello world!"), KC.Macro(Press(KC.LCMD), Tap(KC.S), Release(KC.LCMD)),]
+    [
+        # 1 → Copy (Ctrl + C)
+        KC.MACRO(
+            Press(KC.LCTRL),
+            Tap(KC.C),
+            Release(KC.LCTRL)
+        ),
+
+        # 2 → Paste (Ctrl + V)
+        KC.MACRO(
+            Press(KC.LCTRL),
+            Tap(KC.V),
+            Release(KC.LCTRL)
+        ),
+
+        # 3 → Quick Note (types date + note text)
+        KC.MACRO(
+            "---- Quick Note ----\n",
+            "Date: ",
+            "\n• "
+        ),
+
+        # 4 → Save + Switch App (Ctrl+S then Alt+Tab)
+        KC.MACRO(
+            Press(KC.LCTRL),
+            Tap(KC.S),
+            Release(KC.LCTRL),
+            Press(KC.LALT),
+            Tap(KC.TAB),
+            Release(KC.LALT),
+        ),
+    ]
 ]
 
-# Start kmk!
 if __name__ == '__main__':
     keyboard.go()
